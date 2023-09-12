246216c5f45d2ed5326317d4e819dab58ff25527
Jenni Rivera <jenninrivera@gmail.com>
2023-09-12 12:01:49 -0400

    Complete Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # square_int_list = []
+    # for num in int_list:
+    #     square_int_list.append(num * num)
+    # return square_int_list
+
+    square_int_list = [num * num for num in int_list]
+    return square_int_list
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num) 
+        # else:
+        #     print(num)
+
     pass
